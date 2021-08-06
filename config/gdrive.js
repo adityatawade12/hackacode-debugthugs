e6b6ef393c18b85c35dfe06df6b584a57a13f3dc
Aditya <aditya.tawade@somaiya.edu>
2021-08-07 04:32:00 +0530

    added gdrive

---
+++ config/gdrive.js
@@ -0,0 +1,16 @@
+
+const {google} = require('googleapis');
+require('dotenv').config()
+
+
+const content=process.env.gdrive
+
+const {client_secret, client_id, redirect_uris} = JSON.parse(content).installed;
+const oAuth2Client =new google.auth.OAuth2(
+  client_id, client_secret, redirect_uris[0]);
+const token=process.env.token
+oAuth2Client.setCredentials(JSON.parse(token))
+console.log(oAuth2Client)
+
+module.exports=oAuth2Client
+
